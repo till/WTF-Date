3667797884e478a4e969bebb0c19054f1cb23bb3
till <till@php.net>
2013-01-18 18:21:49 +0100

    display an error message instead of failing with a PHP error

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,7 @@
 <?php
-require dirname(__DIR__) . '/vendor/autoload.php';
+$composer = dirname(__DIR__) . '/vendor/autoload.php';
+if (false === file_exists($composer)) {
+    echo "Autoloader does not exist. Please `./composer.phar install`" . PHP_EOL;
+    exit(1);
+}
+require $composer;
